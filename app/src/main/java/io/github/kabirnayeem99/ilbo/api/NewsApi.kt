afb12c0e621d9665c2736f12247ed67b35cd3490
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-02-11 21:07:45 +0600

    📦 new: sets up retrofit

---
+++ app/src/main/java/io/github/kabirnayeem99/ilbo/api/NewsApi.kt
@@ -0,0 +1,30 @@
+package io.github.kabirnayeem99.ilbo.api
+
+import io.github.kabirnayeem99.ilbo.constants.Constants
+import io.github.kabirnayeem99.ilbo.models.NewsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("/v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = Constants.API_KEY
+    ): Response<NewsResponse>
+
+    @GET("/v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = Constants.API_KEY
+    ): Response<NewsResponse>
+}
