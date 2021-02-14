cbc67f58bafe75b350bf323c30bb41dd141a6d4b
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-02-14 18:28:33 +0600

    🐛 fix: fixes bottom navigation

--- app/src/main/java/io/github/kabirnayeem99/ilbo/ui/MainActivity.kt
+++ app/src/main/java/io/github/kabirnayeem99/ilbo/ui/MainActivity.kt
@@ -2,12 +2,16 @@ package io.github.kabirnayeem99.ilbo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import io.github.kabirnayeem99.ilbo.R
 import io.github.kabirnayeem99.ilbo.db.ArticleDatabase
 import io.github.kabirnayeem99.ilbo.repositories.NewsRepository
+import io.github.kabirnayeem99.ilbo.ui.fragments.BreakingNewsFragment
+import io.github.kabirnayeem99.ilbo.ui.fragments.SavedNewsFragment
+import io.github.kabirnayeem99.ilbo.ui.fragments.SearchNewsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,13 +23,40 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        setUpBottomNavBar()
+
+
         val newsRepository = NewsRepository(ArticleDatabase(this))
         val newsViewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
 
         newsViewModel =
             ViewModelProvider(this, newsViewModelProviderFactory).get(NewsViewModel::class.java)
 
-        bottomNavigationView.setupWithNavController(ilboNavHostFragment.findNavController())
 
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
+
+    private fun setUpBottomNavBar() {
+
+        val breakingNewsFragment = BreakingNewsFragment()
+        val savedNewsFragment = SavedNewsFragment()
+        val searchNewsFragment = SearchNewsFragment()
+
+        makeCurrentFragment(breakingNewsFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.icBreakingNews -> makeCurrentFragment(breakingNewsFragment)
+                R.id.icFavorite -> makeCurrentFragment(savedNewsFragment)
+                R.id.icSearchNews -> makeCurrentFragment(searchNewsFragment)
+            }
+            true
+        }
+    }
+
 }
